f4810dc1117a1d79b9c2cc2b93cf375393a1ead0
hacker <hacker@gmail.com>
2025-02-05 15:58:52 +0700

    Up: test cicd onti local

--- CICD.sh
+++ CICD.sh
@@ -2,11 +2,18 @@
 set -e
 
 APP_NAME=mywebapp
-VERSION=INSECURE
+VERSION=SECURE
 
-#BUILD: Clenaup old container/build image
+echo "scan code"
+
+echo "stage build"
 docker rm -f $APP_NAME 2>/dev/null || true
 docker build -t $APP_NAME:$VERSION . 
 
-#TEST: Run the container
+echo "scan images"
+docker scout cves $APP_NAME:$VERSION --output ./vulns.report
+docker scout cves $APP_NAME:$VERSION --only-severity critical --exit-code
+docker scout sbom --output $APP_NAME.sbom $APP_NAME:$VERSION
+
+echo "Deploy"
 docker run -d -p 80:80 --name webapp $APP_NAME:$VERSION
---
+++ README.md
@@ -0,0 +1,6 @@
+Install Docker Scout
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/docker/scout-cli/main/install.sh -o install-scout.sh
+bash install-scout.sh
+```
